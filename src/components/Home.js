e2c0f5442edcee55b7154d3286187da3115e23b6
Aravindan E <129530507+Aravindan-14@users.noreply.github.com>
2023-12-09 19:47:27 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import FindMe from "./FindMe";
+import Aos from "aos";
 
 function Home() {
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, [])
+
   return (
     <div className="h-full w-full  md:flex justify-center items-center pt-5 md:pt-0">
       <div className="h-full w-full flex flex-col justify-center">
         <span className="nd:text-5xl text-3xl font-bold ">Hi, I'm </span>
-        <h1 className="md:text-[5rem] text-5xl text-pink-600 font-bold md:mb-8 mb-2  mt-2">
+        <h1 className="md:text-[5rem] text-5xl text-pink-600 font-bold md:mb-8 mb-2 mt-2">
           Aravindan
         </h1>
         <span className="md:text-2xl font-bold md:-mt-5 ">
@@ -19,15 +24,18 @@ function Home() {
         </p>
         <FindMe />
       </div>
-      <div className="h-full w-full">
+      <div data-aos="fade-left"
+        className="h-full w-full">
         <img
           className="w-full md:scale-125 mt-12 "
           src="Aravind.png"
           alt=""
         ></img>
       </div>
-    </div>
+    </div >
   );
 }
+
+
 
 export default Home;
