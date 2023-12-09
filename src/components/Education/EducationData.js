6f8e38b8a67d9e2d7288406cb76a6f15937cdbc9
Aravindan E <129530507+Aravindan-14@users.noreply.github.com>
2023-12-09 19:44:13 +0530

    Update EducationData.js

--- src/components/Education/EducationData.js
+++ src/components/Education/EducationData.js
@@ -1,22 +1,24 @@
 import React from "react";
 
-function EducationData() {
+function EducationData(props) {
+
+  const { data } = props
+
+
   return (
     <div className="relative ">
-      <div className=" ml-5 hover:bg-black/90 bg-black/40 mb-5">
+      <div className=" ml-5 hover:bg-black/90 bg-black/40 mb-5 ">
         <div className="border-2 border-pink-600 p-5">
           <div className="pb-2 text-pink-600 ">
             <i class="pr-3 fa-regular fa-calendar"></i>{" "}
-            <span className="font-semibold">2019 - 2022</span>
+            <span className="font-semibold">{data.duration}</span>
           </div>
 
           <h2 className="text- font-bold pb-2">
-            Mohammed Sathan Collage of Art and Science{" "}
+            {data.heading}
           </h2>
           <p className=" text-justify font-semibold text-sm ">
-            I have skills in designing and developing Website. Text in a pre
-            element is displayed in a fixed-width font,I have skills in
-            designing and developing Website.
+            {data.content}
           </p>
         </div>
       </div>
