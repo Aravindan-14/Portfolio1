8ea51d64d0659d0e341a04cbc0141b4544866b52
Aravindan E <129530507+Aravindan-14@users.noreply.github.com>
2023-12-09 19:47:09 +0530

    Update Frontend.js

--- src/components/Frontend.js
+++ src/components/Frontend.js
@@ -1,17 +1,19 @@
 import React from "react";
+import Skill from './Skill';
 
-function Frontend() {
+function Frontend(props) {
+  console.log();
   return (
     <div className="mb-10">
       <div className="flex justify-between items-center mb-2">
-        <span>HTML/CSS</span>
-        <span>90%</span>
+        <span>{props.skillData.skill}</span>
+        <span>{props.skillData.value}%</span>
       </div>
       <div className="w-full relative">
         <div className=" bg-white h-1 w-full absolute "></div>
-        <div className=" bg-pink-600 h-1 w-[80%] absolute "></div>
+        <div style={{ width: `${props.skillData.value}%` }} className=" bg-pink-600 h-1 absolute "></div>
       </div>
-    </div>
+    </div >
   );
 }
 
