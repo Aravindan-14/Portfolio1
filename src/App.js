6d1cbb8dd116e73c40a1dea9adbbaa42ed1b01af
Aravindan E <129530507+Aravindan-14@users.noreply.github.com>
2023-12-09 19:48:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import Education from "./components/Education";
 import Contact from "./components/Contact";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+// ..
+AOS.init();
 function App() {
   return (
     <div className="bg-black/90">
