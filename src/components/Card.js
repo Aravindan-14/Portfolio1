f067952cd8ceb107bdfd55f27b791cc34a66ef7b
Aravindan E <129530507+Aravindan-14@users.noreply.github.com>
2023-12-09 20:20:30 +0530

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,7 +5,7 @@ function Card(props) {
   const { cardData } = props
 
   return (
-    <div data-aos="fade-up" data-aos-duration="1500">
+    <div data-aos="fade-up" data-aos-duration="2000">
       <div className="h-[370px] w-72 bg-black/50 hover:bg-black/80 rounded-xl p-7">
         <div>
           <i className={cardData.className}  ></i>
