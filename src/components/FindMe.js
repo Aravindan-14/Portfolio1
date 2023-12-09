60732ec390a193a396dd453c94bf3c7379c2da3c
Aravindan E <129530507+Aravindan-14@users.noreply.github.com>
2023-12-09 19:46:21 +0530

    Update FindMe.js

--- src/components/FindMe.js
+++ src/components/FindMe.js
@@ -6,16 +6,16 @@ function FindMe() {
       <span className="font-semibold uppercase ">Find with me</span>
       <div className="flex gap-5 mt-3">
         <div className="h-8 w-8 bg-gray-950 flex justify-center items-center rounded text-pink-600 hover:text-white hover:rounded-full">
-          <i class="fa-brands fa-linkedin-in"></i>
+          <a href="http://surl.li/obzbb"><i class="fa-brands fa-linkedin-in"></i></a>
         </div>
         <div className="h-8 w-8 bg-gray-950 flex justify-center items-center rounded text-pink-600 hover:text-white hover:rounded-full">
-          <i class="fa-brands fa-github"></i>
+          <a href="https://github.com/Aravindan-14?tab=repositories"><i class="fa-brands fa-github"></i></a>
         </div>
         <div className="h-8 w-8 bg-gray-950 flex justify-center items-center rounded text-pink-600 hover:text-white hover:rounded-full">
-          <i class="fa-brands fa-facebook"></i>
+          <a href="http://surl.li/obzbt"><i class="fa-brands fa-facebook"></i></a>
         </div>
         <div className="h-8 w-8 bg-gray-950 flex justify-center items-center rounded text-pink-600 hover:text-white hover:rounded-full">
-          <i class="fa-brands fa-instagram"></i>
+          <a href="http://surl.li/obzde"><i class="fa-brands fa-instagram"></i></a>
         </div>
       </div>
     </div>
