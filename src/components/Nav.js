1c112834c5d42ce31da45345570b3e0fd053c872
Aravindan E <129530507+Aravindan-14@users.noreply.github.com>
2023-12-10 20:35:24 +0530

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,10 +6,11 @@ function Nav() {
       <div className="h-16 w-screen bg-black  flex justify-between items-center px-5 md:px-20">
         <h1 className="md:text-3xl font-bold">Aravindan</h1>
         <ul className="hidden md:flex gap-8 font-bold">
-          <a href=""><li className="hover:text-pink-600">Home</li></a>
-          <a href="about"><li className="hover:text-pink-600">About</li></a>
-          <a href="skill"><li className="hover:text-pink-600">Skill</li></a>
-          <a href="education"><li className="hover:text-pink-600">Education</li></a>
+    <li className="hover:text-pink-600">Home</li>
+          <li className="hover:text-pink-600">About</li>
+          <li className="hover:text-pink-600">Skill</li>
+        
+          <li className="hover:text-pink-600">Education</li>
         </ul>
         <div className="md:hidden">
           <i class="fa-solid fa-bars"></i>
