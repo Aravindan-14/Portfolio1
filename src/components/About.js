99c007825e33a1103d5a4c59f953a09ab1ebc4a0
Aravindan E <129530507+Aravindan-14@users.noreply.github.com>
2023-12-10 21:08:10 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Resume from "./Aravindan.pdf"
 
 function About() {
   return (
@@ -23,9 +23,9 @@ function About() {
         <p className=" mb-10 md:pr-28 text-center">
           I am actively seeking opportunities as a full-stack developer. In addition to my technical expertise, I have skills in designing posters and logos, incorporating my editing skills into front-end development. My unique skill set and passion for both coding and design position me as a versatile candidate ready to contribute to innovative projects.
         </p>
-        <button className="uppercase bg-black/50 px-5 py-3 rounded text-pink-600 font-semibold hover:text-white hover:bg-pink-600">
+        <a href={Resume} download="Aravindan Resum" ><button className="uppercase bg-black/50 px-5 py-3 rounded text-pink-600 font-semibold hover:text-white hover:bg-pink-600">
           download cv
-        </button>
+        </button></a>
       </div>
     </div>
   );
