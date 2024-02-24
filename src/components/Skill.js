34a12aa65712617ace3aab3ac0a7a641ed45c75d
Aravindan E <129530507+Aravindan-14@users.noreply.github.com>
2024-02-24 17:51:00 +0530

    Update Skill.js

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -9,16 +9,16 @@ function Skill() {
           My <span className="text-pink-600">Skill</span>
         </h1>
         <div className="flex flex-wrap justify-center gap-5 mb-10">
-          <div data-aos="fade-up" data-aos-duration="1000" className="bg-black/50 h-16 rounded w-48 flex justify-center items-center p-3 hover:bg-gray-800">
+          <div data-aos="fade-up" data-aos-duration="1000" className="bg-white h-16 rounded w-48 flex justify-center items-center p-3 hover:bg-gray-800/95">
             <img src="React.png" alt=""></img>
           </div>
-          <div data-aos="fade-up" data-aos-duration="1000" className="bg-black/50 h-16 rounded w-48 flex justify-center items-center p-3 hover:bg-gray-800">
+          <div data-aos="fade-up" data-aos-duration="1000" className="bg-white h-16 rounded w-48 flex justify-center items-center p-3 hover:bg-gray-800/95">
             <img src="Ps.png" alt=""></img>
           </div>
-          <div data-aos="fade-up" data-aos-duration="1000" className="bg-black/50 h-16 rounded w-48 flex justify-center items-center p-3 hover:bg-gray-800">
+          <div data-aos="fade-up" data-aos-duration="1000" className="bg-white h-16 rounded w-48 flex justify-center items-center p-3 hover:bg-gray-800/95">
             <img src="Fg.png" alt=""></img>
           </div>
-          <div data-aos="fade-up" data-aos-duration="1000" className="bg-black/50 h-16 rounded w-48 flex justify-center items-center p-3 hover:bg-gray-800">
+          <div data-aos="fade-up" data-aos-duration="1000" className="bg-white h-16 rounded w-48 flex justify-center items-center p-3 hover:bg-gray-800/95">
             <img src="Node js.png" alt=""></img>
           </div>
         </div>
